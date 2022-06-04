162ca30d0570211934b6c09a8659e0070b2d1fd9
Pasan Madushan <pasanmadushan@gmail.com>
2022-06-04 22:37:35 +0530

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -45,7 +45,7 @@ export const routes: Routes = [
       },
     ],
   },
-  { path: '', redirectTo: 'pages', pathMatch: 'full' },
+  { path: '', redirectTo: 'auth', pathMatch: 'full' },
   { path: '**', redirectTo: 'pages' },
 ];
 
